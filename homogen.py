57cf3670abc8a9f93e19f3a9c2d5ffc06f1bbec4
Jan Heczko <jan.heczko@gmail.com>
2016-09-10 18:24:14 +0200

    Change command line interface of homogen.py

--- homogen.py
+++ homogen.py
@@ -6,27 +6,36 @@ import sfepy
 from sfepy.base.conf import ProblemConf, get_standard_keywords
 from sfepy.homogenization.homogen_app import HomogenizationApp
 
-help = {
-    'filename' :
-    'basename of output file(s) [default: <basename of input file>]',
+helps = {
+    'conf' :
+    'override problem description file items, written as python'
+    ' dictionary without surrounding braces',
+    'options' : 'override options item of problem description,',
 }
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("--version", action="version",
                         version="%(prog)s " + sfepy.__version__)
-    parser.add_argument("-o", metavar='filename', action="store",
-                        dest="output_filename_trunk",
-                        default=None, help=help['filename'])
+    parser.add_argument('-c', '--conf', metavar='"key : value, ..."',
+                        action='store', dest='conf', type=str,
+                        default=None, help= helps['conf'])
+    parser.add_argument('-O', '--options', metavar='"key : value, ..."',
+                        action='store', dest='app_options', type=str,
+                        default=None, help=helps['options'])
     parser.add_argument('filename_in')
     options = parser.parse_args()
+
+    # the value is not used even if set:
+    options.output_filename_trunk = None
 
     filename_in = options.filename_in
 
     required, other = get_standard_keywords()
     required.remove('equations')
 
-    conf = ProblemConf.from_file(filename_in, required, other)
+    conf = ProblemConf.from_file_and_options(
+        filename_in, options, required, other)
 
     app = HomogenizationApp(conf, options, 'homogen:')
     opts = conf.options
